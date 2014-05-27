36d94149b8e3153db8761c3d0d9c6e831f735eee
Karsten Knese <karsten.knese@googlemail.com>
2014-05-27 16:02:39 +0200

    ignore grasp orientation when impossible

--- src/FeatureGraspingPoint.cpp
+++ src/FeatureGraspingPoint.cpp
@@ -14,9 +14,9 @@ DYNAMICGRAPH_FACTORY_ENTITY_PLUGIN(FeatureGraspingPoint,"FeatureGraspingPoint");
 
 FeatureGraspingPoint::FeatureGraspingPoint(const string& pointName) 
     : FeatureVisualPoint( pointName),
-      distance_threshold_(0.05f),
+      distance_threshold_(0.01f),
       is_closed_(true),
-      auto_closing_(true),
+      auto_closing_(false),
       nh_(),
       ac_(nh_,"right_hand_controller/follow_joint_trajectory", true)
 {
@@ -52,51 +52,54 @@ FeatureGraspingPoint::FeatureGraspingPoint(const string& pointName)
                new dynamicgraph::command::Setter<FeatureGraspingPoint, bool >
                (*this, &FeatureGraspingPoint::enableAutomaticClosing, docstring));
 
-    // create the action client
-    // true causes the client to spin its own thread
-    bool in_time = ac_.waitForServer(ros::Duration(20));
-    if (!in_time)
-    {
-        ROS_ERROR_STREAM("sot grasping could not reach any action server");
+    if (isReferenceSet()){
+
+        // create the action client
+        // true causes the client to spin its own thread
+        bool in_time = ac_.waitForServer(ros::Duration(20));
+        if (!in_time)
+        {
+            ROS_ERROR_STREAM("sot grasping could not reach any action server");
+        }
+        ROS_INFO_STREAM("sot_grasping action server successfully initialized");
+
+        goal_open_ = control_msgs::FollowJointTrajectoryGoal();
+
+        goal_open_.trajectory.joint_names.push_back("hand_right_thumb_joint");
+        goal_open_.trajectory.joint_names.push_back("hand_right_middle_joint");
+        goal_open_.trajectory.joint_names.push_back("hand_right_index_joint");
+
+        trajectory_msgs::JointTrajectoryPoint point;
+        point.time_from_start = ros::Duration(4);
+
+        point.positions.push_back(0.1);
+        point.positions.push_back(0.1);
+        point.positions.push_back(0.1);
+
+        point.velocities.push_back(0);
+        point.velocities.push_back(0);
+        point.velocities.push_back(0);
+
+        goal_open_.trajectory.points.push_back(point);
+
+        for (int i=0;i<goal_open_.trajectory.joint_names.size();++i)
+        {
+            control_msgs::JointTolerance tol;
+            tol.name = goal_open_.trajectory.joint_names[i];
+            tol.position = 5;
+            tol.velocity = 5;
+            tol.acceleration = 5;
+            goal_open_.goal_tolerance.push_back(tol);
+        }
+        goal_open_.goal_time_tolerance = ros::Duration(3);
+
+        goal_close_ = control_msgs::FollowJointTrajectoryGoal(goal_open_);
+        goal_close_.trajectory.points[0].positions[0] = 1.5;
+        goal_close_.trajectory.points[0].positions[1] = 4.0;
+        goal_close_.trajectory.points[0].positions[2] = 4.0;
+
+        openHand();
     }
-    ROS_INFO_STREAM("sot_grasping action server successfully initialized");
-
-    goal_open_ = control_msgs::FollowJointTrajectoryGoal();
-
-    goal_open_.trajectory.joint_names.push_back("hand_right_thumb_joint");
-    goal_open_.trajectory.joint_names.push_back("hand_right_middle_joint");
-    goal_open_.trajectory.joint_names.push_back("hand_right_index_joint");
-
-    trajectory_msgs::JointTrajectoryPoint point;
-    point.time_from_start = ros::Duration(4);
-
-    point.positions.push_back(0.1);
-    point.positions.push_back(0.1);
-    point.positions.push_back(0.1);
-
-    point.velocities.push_back(0);
-    point.velocities.push_back(0);
-    point.velocities.push_back(0);
-
-    goal_open_.trajectory.points.push_back(point);
-
-    for (int i=0;i<goal_open_.trajectory.joint_names.size();++i)
-    {
-        control_msgs::JointTolerance tol;
-        tol.name = goal_open_.trajectory.joint_names[i];
-        tol.position = 5;
-        tol.velocity = 5;
-        tol.acceleration = 5;
-        goal_open_.goal_tolerance.push_back(tol);
-    }
-    goal_open_.goal_time_tolerance = ros::Duration(3);
-
-    goal_close_ = control_msgs::FollowJointTrajectoryGoal(goal_open_);
-    goal_close_.trajectory.points[0].positions[0] = 1.5;
-    goal_close_.trajectory.points[0].positions[1] = 4.0;
-    goal_close_.trajectory.points[0].positions[2] = 4.0;
-
-    openHand();
 }
 
 void FeatureGraspingPoint::openHand(){
@@ -178,10 +181,6 @@ computeJacobian( ml::Matrix& J,int time )
     // xy point in 2D
     const double & x = xySIN(time)(0);
     const double & y = xySIN(time)(1);
-
-    std::cerr << "Z " << Z << std::endl;
-    std::cerr << "x " << x << std::endl;
-    std::cerr << "y " << y << std::endl;
 
     if( Z<0 )
     {
